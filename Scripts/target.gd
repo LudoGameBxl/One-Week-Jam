7937023ebcb01e174a9d9e009f1916a9a4714fed
Ludo <ludovic-haute@hotmail.com>
2025-02-19 01:52:47 +0100

    change target to rigidbody element + add physic, ...

--- Scripts/target.gd
+++ Scripts/target.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-
-
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -11,7 +9,7 @@ func _process(delta: float) -> void:
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("bullet"):
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "rotation_degrees", Vector3(-90, 0,0 ), 1)
+		#var tween = get_tree().create_tween()
+		#tween.tween_property(self, "scale", Vector3(0, 0,0 ), 1)
 		#rotation_degrees.x = -90
 		print(body.bullet_type)
